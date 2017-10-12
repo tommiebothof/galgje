ca4aaf325daf299754a83a301639cfef54ef3726
tommie <tommie.bothof@coderclass.nl>
2017-10-12 10:29:49 +0200

    galgje nog zonder counter

--- galg.py
+++ galg.py
@@ -1,17 +1,25 @@
 import random
+print ("welkom bij galgje")
+fletter = ""
 woord = ["game", "loop", "raar"]
 gwoord = (random.choice(woord))
 streep = ("_ _ _ _")
 print (streep)
 while True:
-    letter1 = str(input("geef een leter"))
+    letter1 = str(input("geef een letter"))
     if letter1 in gwoord:
         print("goed")
+    elif len(letter1) >= 2:
+        print("De letter is te lang, als je het hele woord wil invoeren vul dan ? in.")
     elif letter1 == "?":
         print("weet jij het woord")
         antwoord = str(input("typ het woord maar"))
-        if antwoord == gwoord:
+        if antwoord != gwoord:
+            print("helaas niet goed, probeer het opnieuw")
+        elif antwoord == gwoord:
             print("goed gedaan")
             break
     elif letter1 != gwoord:
+        counter +=1
         print ("niet goed")
+        print ("dit zijn je foute letters:"+fletter)
